e6c8b48f42ed1f85cae0314e2160c07f09a51797
Bengi Mizrahi <bengimizrahi@gmail.com>
2017-10-19 10:44:58 +0300

    Fix this: Need to check if point is close to line

--- PenAndPaper/StrokeDataModel.swift
+++ PenAndPaper/StrokeDataModel.swift
@@ -32,7 +32,14 @@ class Stroke {
             let v1 = vertices[i + 1].location
 
             if distance(from: point, to: (v0, v1)) <= kOverlapRegionWidth {
-                return true
+                let px_lte_v0x = point.x <= v0.x
+                let px_lte_v1x = point.x <= v1.x
+                let py_lte_v0y = point.y <= v0.y
+                let py_lte_v1y = point.y <= v1.y
+
+                if px_lte_v0x != px_lte_v1x || py_lte_v0y != py_lte_v1y {
+                    return true
+                }
             }
         }
 
